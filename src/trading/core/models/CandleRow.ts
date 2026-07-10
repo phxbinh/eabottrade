b3cc74db682bb136ade617cf220de7b526d495c1
phxbinh <phxbinh@gmail.com>
2026-07-10 13:51:33 +0700

    Update CandleRow.ts

--- src/trading/core/models/CandleRow.ts
+++ src/trading/core/models/CandleRow.ts
@@ -2,6 +2,7 @@
 // Sử dụng cho huấn luyện và luyện tập
 // app/training-guide/page.tsx
 
+/*
 import { Candle } from "./Candle";
 
 export interface CandleRow extends Candle {
@@ -9,4 +10,19 @@ export interface CandleRow extends Candle {
   macd: number | null;
   signal: number | null;
   histogram: number | null;
+}
+*/
+
+import { Candle } from "./Candle";
+
+export interface CandleRow extends Candle {
+  rsi: number | null;
+  macd: number | null;
+  signal: number | null;
+  histogram: number | null;
+  ema20: number | null;
+  ema50: number | null;
+  plusDI: number | null;
+  minusDI: number | null;
+  adx: number | null;
 }
